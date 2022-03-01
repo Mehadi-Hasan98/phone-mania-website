40db6e04488e13198e84af2ebb5a1e094f141961
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-03-01 11:38:09 +0600

    phone details added

--- js/phone.js
+++ js/phone.js
@@ -1,22 +1,63 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    // clear data
+    searchField.value = '';
+
+    // load data
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displaySearchResult(data.data));
 }
 
-const displaySearchResult = phones => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    if(phones.length == 0){
-        // show no result found
-    }
-    phones.forEach(phone => {
-        console.log(phone);
+const displaySearchResult = data => {
+      const searchResult = document.getElementById('search-result');
+      searchResult.textContent = '';
+    // if(phones.length == 0){
+    //     // show no result found
+    // }
+     data.forEach(phone => {
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = ``
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+                <h6 class="card-title">Brand: ${phone.brand}</h6>
+                <button onclick="loadPhoneDetail('${phone.slug}')">Details</button>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data.data));
+}
+
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top width=200px" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Phone Name: ${phone.name}</h5>
+        <h6 class="card-title">Brand: ${phone.brand}</h6>
+        <p class="card-text">Release Date: ${phone.releaseDate}.</p>
+        <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}.</p>
+        <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}.</p>
+        <p class="card-text">Memory: ${phone.mainFeatures.memory}.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
 }
